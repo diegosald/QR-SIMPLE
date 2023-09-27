cbafb3a4ce3d3bc3bcb06e3ffa434d4b05096c54
diegosald <diegosalday@gmail.com>
2023-09-26 23:47:04 -0300

    act

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function generar(){
     if (texto.value != "") {
         document.getElementById("foto").src = objconec.url+texto.value
         document.getElementById("foto").style = "visibility:visible;"
-       /* document.getElementById("msg").innerHTML = ""*/
+      document.getElementById("msg").innerHTML = ""
     }
     else{
         document.getElementById("msg").innerHTML = "texto no puede ser vacio"
@@ -19,7 +19,7 @@ function generar(){
 }
 
 function limpiar(){
-       /* document.getElementById("msg").innerHTML = ""*/
+        document.getElementById("msg").innerHTML = ""
         document.getElementById("texto").value = ""
         document.getElementById("foto").src = ""
         document.getElementById("foto").style = "visibility:hidden;"
